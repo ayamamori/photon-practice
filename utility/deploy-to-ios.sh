7b4dc40520e510fde08f1180d064fc0595d02a22
yamamori.akihiro <yamamori.akihiro@donuts.ne.jp>
2016-05-30 11:36:56 +0900

    Generated batchmode build directory

--- utility/deploy-to-ios.sh
+++ utility/deploy-to-ios.sh
@@ -1,29 +1,35 @@
-PROJECT_BASE=`pwd`/`dirname $(dirname $BASH_SOURCE)`
+PROJECT_UTILITY_DIR=`pwd`/`dirname $BASH_SOURCE`
+PROJECT_BASE=`dirname $PROJECT_UTILITY_DIR`
 APP_NAME=photon-practice.app
 
+cd $PROJECT_UTILITY_DIR
+
 echo "Copying sources"
-rm -r Assets
-rm -r ProjectSettings
-cp -r ~/git/photon-practice/Assets/ Assets/
-cp -r ~/git/photon-practice/ProjectSettings/ ProjectSettings
+rm -r Assets 2>/dev/null
+rm -r ProjectSettings 2>/dev/null
+cp -r $PROJECT_BASE/Assets/ Assets/
+cp -r $PROJECT_BASE/ProjectSettings/ ProjectSettings/
 
 echo "Building Unity"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -projectPath $PROJECT_BASE -executeMethod BatchBuild.BuildProject_iOS -quit
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -projectPath $PROJECT_UTILITY_DIR -executeMethod BatchBuild.BuildProject_iOS -quit
 if [ $? != 0 ]; then
   echo "Unity Build failed"
   exit $?
 fi
 
 echo "Building Xcode Project"
-xcodebuild -project $PROJECT_BASE/Device/Unity-iPhone.xcodeproj -target Unity-iPhone -configuration Release build
+xcodebuild -project $PROJECT_UTILITY_DIR/Device/Unity-iPhone.xcodeproj -target Unity-iPhone -configuration Release build
 if [ $? != 0 ]; then
   echo "Xcode Build failed"
   exit $?
 fi
 
 echo "Deploying to the device"
-ios-deploy --justlaunch --debug --bundle $PROJECT_BASE/Device/build/Release-iphoneos/$APP_NAME
-if [ $? != 0 ]; then
+ios-deploy --debug --bundle $PROJECT_UTILITY_DIR/Device/build/Release-iphoneos/$APP_NAME
+if [ $? -eq 254 ]; then
+  echo "Deploying finished, but couldn't run app"
+  exit $?
+elif [ $? -ne 0 ]; then
   echo "Deploying failed"
   exit $?
 fi
