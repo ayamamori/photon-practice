65343610c91ec1d40dca2cab2f795a37ef01ccd9
yamamori.akihiro <yamamori.akihiro@donuts.ne.jp>
2016-06-01 16:22:24 +0900

    Updated

---
+++ Assets/Scripts/PlaneRPC.cs
@@ -0,0 +1,15 @@
+﻿using Photon;
+using UnityEngine;
+
+public class PlaneRPC : Photon.PunBehaviour {
+    int i = 0;
+
+    void OnMouseDown() {
+        photonView.RPC("ClickClickClickClickClick", PhotonTargets.All, i++);
+    }
+
+    [PunRPC]
+    void ClickClickClickClickClick(int i) {
+        Debug.Log(i);
+    }
+}
--- utility/deploy-to-ios.sh
+++ utility/deploy-to-ios.sh
@@ -1,8 +1,10 @@
-PROJECT_UTILITY_DIR=`pwd`/`dirname $BASH_SOURCE` # <= must be absolute path
-PROJECT_BASE=`dirname $PROJECT_UTILITY_DIR`
+#Locate this script under an empty directory.
+
+PROJECT_BUILD_DIR=`pwd`/`dirname $BASH_SOURCE` # <= must be absolute path
+PROJECT_BASE=~/git/photon-practice/
 APP_NAME=photon-practice.app
 
-cd $PROJECT_UTILITY_DIR
+cd $PROJECT_BUILD_DIR
 
 echo "Copying sources"
 rm -r Assets 2>/dev/null
@@ -11,21 +13,21 @@ cp -r $PROJECT_BASE/Assets/ Assets/
 cp -r $PROJECT_BASE/ProjectSettings/ ProjectSettings/
 
 echo "Building Unity"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -projectPath $PROJECT_UTILITY_DIR -executeMethod BatchBuild.BuildProject_iOS -quit
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -projectPath $PROJECT_BUILD_DIR -executeMethod BatchBuild.BuildProject_iOS -quit
 if [ $? != 0 ]; then
   echo "Unity Build failed"
   exit $?
 fi
 
 echo "Building Xcode Project"
-xcodebuild -project $PROJECT_UTILITY_DIR/Device/Unity-iPhone.xcodeproj -target Unity-iPhone -configuration Release build
+xcodebuild -project $PROJECT_BUILD_DIR/Device/Unity-iPhone.xcodeproj -target Unity-iPhone -configuration Release build
 if [ $? != 0 ]; then
   echo "Xcode Build failed"
   exit $?
 fi
 
 echo "Deploying to the device"
-ios-deploy --debug --bundle $PROJECT_UTILITY_DIR/Device/build/Release-iphoneos/$APP_NAME
+ios-deploy --debug --bundle $PROJECT_BUILD_DIR/Device/build/Release-iphoneos/$APP_NAME
 if [ $? -eq 254 ]; then
   echo "Deploying finished, but couldn't run app"
   exit $?
